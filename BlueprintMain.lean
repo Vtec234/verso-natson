5bb270af253e231df871b4296301743518f2b786
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-06-11 17:48:23 +0200

    Pin blueprint numbering options

--- BlueprintMain.lean
+++ BlueprintMain.lean
@@ -5,6 +5,10 @@ import CarlesonBlueprint
 open Verso Doc
 open Verso.Genre Manual
 
+set_option verso.blueprint.numbering sub
+set_option verso.blueprint.subNumberingPrefix full
+set_option verso.blueprint.subNumberingCounter prefix
+
 def main (args : List String) : IO UInt32 :=
   Informal.PreviewManifest.manualMainWithPreviewData
     (%doc CarlesonBlueprint)
