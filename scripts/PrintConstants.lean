de731a7d8c8e6f828aff667752689935cc9fcb46
Wojciech Nawrocki <wjn@lean-fro.org>
2026-08-10 21:33:43 +0000

    feat: constant list

---
+++ scripts/PrintConstants.lean
@@ -0,0 +1,21 @@
+import Mathlib
+import Lean.Elab.Command
+open Lean
+
+run_cmd do
+  let env ← getEnv
+  let mut consts := #[]
+  for (n, ci) in env.constants do
+    -- Exclude non-theorems and internal theorems.
+    if !ci.isTheorem || n.isInternalDetail then continue
+    let some imod := env.getModuleIdxFor? n | continue
+    let mod := env.header.moduleNames[imod]!
+    -- Exclude theorems not in mathlib.
+    -- if mod.getRoot != `Mathlib then continue
+    -- Exclude auto-generated theorems by looking for a source range.
+    if (← findDeclarationRangesCore? n).isNone then continue
+    consts := consts.push (n, mod)
+  -- 160 constants are needed for Main.lean.
+  let chosen := consts.qsort (fun (a, _) (b, _) => a.hash < b.hash) |>.take 160
+  for (n, mod) in chosen do
+    IO.println s!"{mod} | {n}"
